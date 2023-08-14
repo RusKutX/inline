846ca1f7ccd0d4e18fe20f750966e499769d3b75
Ruslan Kutergin <stalk.2116@gmail.com>
2023-08-14 13:35:58 +0300

    заменил на кам

--- inline/Source.cpp
+++ inline/Source.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 //inline рекомендуется использовать в маленьких и простых функциях 
 
-inline int Sum(int a, int b)
+inline int cum(int a, int b)
 {
 	return a + b;
 }
 
 int main()
 {
-	int a = Sum(3, 5);
+	int a = cum(3, 5);
 	cout << a << endl;
 }
